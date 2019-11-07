393f8799664f696c62071cbcda79086bca56439d
Bob Greenshields <bob@thegreenshields.com>
2019-11-07 18:11:19 +0000

    Add istari.js file and reference in head include

---
+++ assets/js/istari.js
@@ -0,0 +1,17 @@
+$( function() {
+	$( "#accordion" ).accordion({
+		heightStyle: "content",
+		collapsible: true,
+		active: false
+	});
+} );
+
+$( function() {
+	$( "#tabs" ).tabs({
+		collapsible: true
+	});
+} );
+
+$( function() {
+	$( document ).tooltip();
+} );
